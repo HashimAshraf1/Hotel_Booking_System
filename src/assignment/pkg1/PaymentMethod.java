8e08efb2f05f3242394dbf6bb4d1cd8caf082352
PurpleCoke240 <purplecoke240@gmail.com>
2024-10-16 17:53:18 +1300

    Changes Signed-off-by:PurpleCoke240 <purplecoke240@gmail.com>

--- src/assignment/pkg1/PaymentMethod.java
+++ src/assignment/pkg1/PaymentMethod.java
@@ -9,8 +9,25 @@ package assignment.pkg1;
  * @author hash
  */
 public enum PaymentMethod {
-    CREDIT_CARD,
-    DEBIT_CARD,
-    CASH,
-    
+    CREDIT_CARD {
+        @Override
+        public void processPayment(double amount) {
+            System.out.println("Processing credit card payment of: " + amount);
+        }
+    },
+    DEBIT_CARD {
+        @Override
+        public void processPayment(double amount) {
+            System.out.println("Processing debit card payment of: " + amount);
+        }
+    },
+    CASH {
+        @Override
+        public void processPayment(double amount) {
+            System.out.println("Processing cash payment of: " + amount);
+        }
+    };
+
+    public abstract void processPayment(double amount);
 }
+
