fdee5af70100e5858ed5d38b5c2923d099940b15
Kyoung Won <132116564+PurpleCoke240@users.noreply.github.com>
2024-10-23 22:17:38 +1300

    Update AllBookingsDialog.java

--- src/assignment/pkg1/AllBookingsDialog.java
+++ src/assignment/pkg1/AllBookingsDialog.java
@@ -11,11 +11,11 @@ public class AllBookingsDialog extends JDialog {
         
         // Create a panel for booking details with a scroll pane
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));  // Stack elements vertically
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add padding
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));  
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        panel.add(new JLabel("<html><h2>All Bookings:</h2></html>", SwingConstants.CENTER));  // Add title
-        panel.add(Box.createVerticalStrut(10));  // Space between title and content
+        panel.add(new JLabel("<html><h2>All Bookings:</h2></html>", SwingConstants.CENTER)); 
+        panel.add(Box.createVerticalStrut(10));  
         panel.add(new JSeparator());
 
         if (bookings.isEmpty()) {
@@ -26,17 +26,19 @@ public class AllBookingsDialog extends JDialog {
 
                 // Use HTML for cleaner formatting of booking details
                 JLabel bookingDetails = new JLabel("<html>" +
-                        "<b>Booking ID:</b> " + booking.getBookingID() + "<br>" +
-                        "<b>Customer:</b> " + booking.getCustomer().getName() + "<br>" +
-                        "<b>Room:</b> " + booking.getRoom().getRoomNumber() + " (" + booking.getRoom().getRoomType() + ")<br>" +
-                        "<b>Start Date:</b> " + booking.getStartDate() + "<br>" +
-                        "<b>End Date:</b> " + booking.getEndDate() + "<br>" +
-                        "<b>Cancelled:</b> " + (booking.isCancelled() ? "Yes" : "No") + "<br>" +
-                        "<b>Payment:</b> " + booking.getPayment().getAmount() + " (" + booking.getPayment().getClass().getSimpleName() + ")<br>" +
-                        "</html>");
+                    "<b>Booking ID:</b> " + booking.getBookingID() + "<br>" +
+                    "<b>Customer:</b> " + booking.getCustomer().getName() + "<br>" +
+                    "<b>Contact Info:</b> " + booking.getCustomer().getContactInfo() + "<br>" + // Add contact info here
+                    "<b>Room:</b> " + booking.getRoom().getRoomNumber() + " (" + booking.getRoom().getRoomType() + ")<br>" +
+                    "<b>Start Date:</b> " + booking.getStartDate() + "<br>" +
+                    "<b>End Date:</b> " + booking.getEndDate() + "<br>" +
+                    "<b>Cancelled:</b> " + (booking.isCancelled() ? "Yes" : "No") + "<br>" +
+                    "<b>Payment:</b> " + booking.getPayment().getAmount() + " (" + booking.getPayment().getClass().getSimpleName() + ")<br>" +
+                    "</html>");
+
 
                 bookingPanel.add(bookingDetails);
-                bookingPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));  // Add padding around booking entries
+                bookingPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0)); 
                 panel.add(bookingPanel);
 
                 // Add separator between bookings
